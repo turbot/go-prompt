d736b1ddc4e153ef28041382ce6537a8bbbad572
Binaek Sarkar <binaek@turbot.com>
2021-02-04 14:01:42 +0530

    updating unit test

--- render_test.go
+++ render_test.go
@@ -69,6 +69,92 @@ func TestFormatCompletion(t *testing.T) {
 	}
 }
 
+func TestToPos(t *testing.T) {
+	type Coordinate struct {
+		x int
+		y int
+	}
+	scenarioTable := []struct {
+		scenario string
+		cols     int
+		text     string
+		cursor   int
+		expected Coordinate
+	}{
+		{
+			scenario: "Single line",
+			cols:     20,
+			text:     "0123456",
+			cursor:   3,
+			expected: Coordinate{3, 0},
+		},
+		{
+			scenario: "Multi line",
+			cols:     20,
+			text:     "0123456\n0\n01234\n0123456789",
+			cursor:   13,
+			expected: Coordinate{3, 2},
+		},
+		{
+			scenario: "Multi line with overflow in one line",
+			cols:     20,
+			text:     "0123456\n012345678901234567890123\n01234\n0123456789",
+			cursor:   13,
+			expected: Coordinate{5, 1},
+		},
+		{
+			scenario: "Multi line with overflow in two line",
+			cols:     20,
+			text:     "0123456\n012345678901234567890123\n01234\n012345678901234567890123",
+			cursor:   13,
+			expected: Coordinate{5, 1},
+		},
+		{
+			scenario: "Multi line with overflow in two line",
+			cols:     20,
+			text:     "0123456\n012345678901234567890123\n01234\n012345678901234567890123",
+			cursor:   0,
+			expected: Coordinate{0, 0},
+		},
+	}
+
+	r := &Render{
+		prefix: "> ",
+		out: &PosixWriter{
+			fd: syscall.Stdin, // "write" to stdin just so we don't mess with the output of the tests
+		},
+		livePrefixCallback:           func() (string, bool) { return "", false },
+		prefixTextColor:              Blue,
+		prefixBGColor:                DefaultColor,
+		inputTextColor:               DefaultColor,
+		inputBGColor:                 DefaultColor,
+		previewSuggestionTextColor:   Green,
+		previewSuggestionBGColor:     DefaultColor,
+		suggestionTextColor:          White,
+		suggestionBGColor:            Cyan,
+		selectedSuggestionTextColor:  Black,
+		selectedSuggestionBGColor:    Turquoise,
+		descriptionTextColor:         Black,
+		descriptionBGColor:           Turquoise,
+		selectedDescriptionTextColor: White,
+		selectedDescriptionBGColor:   Cyan,
+		scrollbarThumbColor:          DarkGray,
+		scrollbarBGColor:             Cyan,
+		col:                          1,
+	}
+
+	for _, s := range scenarioTable {
+		r.col = uint16(s.cols)
+		x, y := r.toPos(s.cursor, s.text)
+
+		coord := Coordinate{x, y}
+
+		if !reflect.DeepEqual(coord, s.expected) {
+			t.Errorf("Should be %#v, but got %#v", s.expected, coord)
+		}
+	}
+}
+
 func TestBreakLineCallback(t *testing.T) {
 	var i int
 	r := &Render{
